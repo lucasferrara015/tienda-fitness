c5b6d8c9cd55c0baa8a3ebd3aa19a207578ce296
lucasferrara015 <luca015ferrara@gmail.com>
2026-03-23 19:44:46 -0300

    Add files via upload

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+// src/index.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"; // si tienes estilos globales, impórtalos aquí
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+// src/types.ts
+export interface Producto {
+  id: number;
+  nombre: string;
+  descripcion: string;
+  precio: number;
+  imagen: string;
+}
